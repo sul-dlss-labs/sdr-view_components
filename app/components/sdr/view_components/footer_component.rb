53a244763c90291b7bdf1293cbd081a3e006bad9
Aaron Collier <aaron.collier@stanford.edu>
2025-11-05 14:59:50 -0800

    Add SDR footer component

---
+++ app/components/sdr/view_components/footer_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Sdr
+  module ViewComponents
+    # Component for rendering page footer.
+    class FooterComponent < ViewComponent::Base
+    end
+  end
+end
